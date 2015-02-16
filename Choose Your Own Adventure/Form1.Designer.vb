208e9d1f9819de85a7b78bf44f87c9de5290ee2f
huw <huw1999@gmail.com>
2015-02-16 14:07:53 +1100

    Initial

---
+++ Choose Your Own Adventure/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayBox = New System.Windows.Forms.RichTextBox()
+        Me.Choice1 = New System.Windows.Forms.Button()
+        Me.Choice2 = New System.Windows.Forms.Button()
+        Me.Choice3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'DisplayBox
+        '
+        Me.DisplayBox.BackColor = System.Drawing.SystemColors.MenuText
+        Me.DisplayBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DisplayBox.Font = New System.Drawing.Font("Source Code Pro Medium", 12!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.DisplayBox.ForeColor = System.Drawing.Color.Lime
+        Me.DisplayBox.Location = New System.Drawing.Point(12, 12)
+        Me.DisplayBox.Name = "DisplayBox"
+        Me.DisplayBox.Size = New System.Drawing.Size(619, 266)
+        Me.DisplayBox.TabIndex = 0
+        Me.DisplayBox.Text = ""
+        '
+        'Choice1
+        '
+        Me.Choice1.Location = New System.Drawing.Point(12, 284)
+        Me.Choice1.Name = "Choice1"
+        Me.Choice1.Size = New System.Drawing.Size(202, 50)
+        Me.Choice1.TabIndex = 1
+        Me.Choice1.Text = "Button1"
+        Me.Choice1.UseVisualStyleBackColor = true
+        '
+        'Choice2
+        '
+        Me.Choice2.Location = New System.Drawing.Point(220, 284)
+        Me.Choice2.Name = "Choice2"
+        Me.Choice2.Size = New System.Drawing.Size(202, 50)
+        Me.Choice2.TabIndex = 2
+        Me.Choice2.Text = "Button2"
+        Me.Choice2.UseVisualStyleBackColor = true
+        '
+        'Choice3
+        '
+        Me.Choice3.Location = New System.Drawing.Point(428, 284)
+        Me.Choice3.Name = "Choice3"
+        Me.Choice3.Size = New System.Drawing.Size(203, 50)
+        Me.Choice3.TabIndex = 3
+        Me.Choice3.Text = "Button3"
+        Me.Choice3.UseVisualStyleBackColor = true
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = true
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(643, 346)
+        Me.Controls.Add(Me.Choice3)
+        Me.Controls.Add(Me.Choice2)
+        Me.Controls.Add(Me.Choice1)
+        Me.Controls.Add(Me.DisplayBox)
+        Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(false)
+
+End Sub
+
+    Friend WithEvents DisplayBox As RichTextBox
+    Friend WithEvents Choice1 As Button
+    Friend WithEvents Choice2 As Button
+    Friend WithEvents Choice3 As Button
+End Class
---
+++ Choose Your Own Adventure/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim path As String = ""
+    Dim rooms() As Room
+
+    'Room contains all of our room data. It's great because it's easy to expand
+    '   if we need to.
+
+    Structure Room
+        Dim id As Integer
+        Dim name As String
+        Dim scenario As String
+        Dim choice1 As Integer
+        Dim text1 As String
+        Dim choice2 As Integer
+        Dim text2 As String
+        Dim choice3 As Integer
+        Dim text3 As String
+    End Structure
+
+    'Load_File will load our game file into the rooms dynamic array, and define
+    '   its length based on our needs
+
+    Private Sub Load_File()
+
+        Dim stream As New System.IO.StreamReader(path)
+        Dim text As String = ""
+        Dim tmp
+
+        ReDim rooms(System.IO.File.ReadLines(path).Count()) As Room
+
+        Dim i As Integer
+        While stream.Peek() <> -1
+            text = stream.ReadLine()
+            tmp = Split(text, "|")
+            rooms(i).id = Int(tmp(0))
+            rooms(i).name = tmp(1)
+            rooms(i).scenario = tmp(2)
+            rooms(i).choice1 = Int(tmp(3))
+            rooms(i).text1 = tmp(4)
+            rooms(i).choice2 = Int(tmp(5))
+            rooms(i).text2 = tmp(6)
+            rooms(i).choice3 = Int(tmp(7))
+            rooms(i).text3 = tmp(8)
+            i += 1
+        End While
+    End Sub
+
+    'Button_Click fires every time the buttons are clicked. It's used to change
+    '   the room or perform other actions that require button clicks. Because
+    '   the function effectively operates the same for each button, we can use
+    '   the 'tag' property of the button to 
+
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Choice1.Click, Choice2.Click, Choice3.Click
+        Load_Room(sender.tag)
+    End Sub
+
+    Private Sub Load_Room(id As Integer)
+
+      DisplayBox.Text = rooms(id).scenario.Replace("**", vbNewLine)
+      Choice1.Text = rooms(id).text1
+      Choice1.Tag = rooms(id).choice1
+      Choice2.Text = rooms(id).text2
+      Choice2.Tag = rooms(id).choice2
+      Choice3.Text = rooms(id).text3
+      Choice3.Tag = rooms(id).choice3
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If System.IO.File.Exists(Application.StartupPath & "\text.txt") Then
+            path = Application.StartupPath & "\text.txt"
+        Else
+            MsgBox("No game file found! Please put it at: " & Application.StartupPath, MsgBoxStyle.Critical, "Error")
+            End
+        End If
+
+        Load_File()
+        Load_Room(0)
+
+    End Sub
+End Class
